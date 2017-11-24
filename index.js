7d4886f10bba9fb41b5a4e09691068228b5ca76b
Ray Blasko <mray6288@gmail.com>
2017-11-24 03:11:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ function theBeatlesPlay(musicians, instruments){
   sentences = []
   for (i = 0; i < musicians.length; i++){
     sentences.push(`${musicians[i]} plays ${instruments[i]}`)
-    console.log(`${musicians[i]} plays ${instruments[i]}`)
+    // console.log(`${musicians[i]} plays ${instruments[i]}`)
   }
-  console.log(sentences)
+  // console.log(sentences)
   return sentences
 }
 
 
 
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
-theBeatlesPlay(musicians, instruments)
+// const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+// const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+// 
+// theBeatlesPlay(musicians, instruments)
