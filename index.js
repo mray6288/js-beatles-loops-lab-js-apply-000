be75bf5613528f669c4d31e33681096f3e8f49e2
Ray Blasko <mray6288@gmail.com>
2017-11-24 03:10:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   sentences = []
   for (i = 0; i < musicians.length; i++){
-    sentences.append(`${musicians[i]} plays ${instruments[i]}`)
+    sentences.push(`${musicians[i]} plays ${instruments[i]}`)
     console.log(`${musicians[i]} plays ${instruments[i]}`)
   }
   console.log(sentences)
