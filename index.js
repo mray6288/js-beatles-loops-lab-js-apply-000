c88dcc39fa2aba026e655908400fd1a6d3be6435
Ray Blasko <mray6288@gmail.com>
2017-11-24 03:12:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments){
 
 
 
-// const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-// const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-//
-// theBeatlesPlay(musicians, instruments)
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+theBeatlesPlay(musicians, instruments)
