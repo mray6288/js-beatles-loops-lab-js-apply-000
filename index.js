32153a12924e123bf0ba545f754d8ab8d15eaa0f
Ray Blasko <mray6288@gmail.com>
2017-11-24 03:13:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians, instruments){
-  sentences = []
+  var sentences = []
   for (i = 0; i < musicians.length; i++){
     sentences.push(`${musicians[i]} plays ${instruments[i]}`)
     // console.log(`${musicians[i]} plays ${instruments[i]}`)
